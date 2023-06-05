8571cd7098afffdb379eb7a8b577662b5b0c9659
Luna Nicolas <luna.nicolas@hotmail.fr>
2023-06-05 13:31:16 +0000

    staging tables from rights now

---
+++ models/stg_logs.sql
@@ -0,0 +1,6 @@
+SELECT 
+user as user_id
+,login_date
+,login_successful
+
+FROM `rightsnow-385413.projet_wagon.logs` 
---
+++ models/stg_users.sql
@@ -0,0 +1,14 @@
+SELECT
+    id AS user_id,
+    LOWER(email) AS email,
+    LOWER(first_name) AS first_name,
+    LOWER(last_name) AS last_name,
+    track_count,
+    broadcast_count,
+    --active,
+    verified,
+    archived,
+    Date_d_inscription AS sub_date,
+    last_modification_ts
+  FROM
+    `rightsnow-385413.projet_wagon.users`
