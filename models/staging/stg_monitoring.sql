08b878ddf1f52cca456baa9a0db667d717d3ae62
Luna Nicolas <luna.nicolas@hotmail.fr>
2023-06-06 07:53:48 +0000

    remove iso_date column and change presta_id column name

--- models/staging/stg_monitoring.sql
+++ models/staging/stg_monitoring.sql
@@ -2,12 +2,12 @@ SELECT
 id as monitoring_id
 --,active
 --,archived
-,channel_id as presta_id
+,channel_id as presta_channel_id
 ,channel_name
 ,channel_type
 ,creation_ts
 --,internal_comment
-,iso_date_utc
+--,iso_date_utc
 --,last_modification_ts
 ,played_duration
 ,track as track_id
