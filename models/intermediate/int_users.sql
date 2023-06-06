d4fbca6ba7f9548a9ac78b8dc156d471d7126d5c
Luna Nicolas <luna.nicolas@hotmail.fr>
2023-06-06 07:44:30 +0000

    update user category with non verified account category and deleted account category

--- models/intermediate/int_users.sql
+++ models/intermediate/int_users.sql
@@ -2,6 +2,8 @@ SELECT
     *,
     CASE
       WHEN REGEXP_CONTAINS(email, 'rightsnow|nicolas.dussart@gmail.com') THEN 'rightsnow'
+      WHEN verified is null then 'non_verified_account'
+      WHEN archived = 1 THEN 'deleted_account'
       WHEN track_count = 0 THEN 'viewers'
       WHEN (broadcast_count / track_count) > 50 OR track_count > 25 THEN 'curious_users'
     ELSE
