b4feee601e25ac2a4be9f81f90c0bae826cd2009
flo coa <coaflorian@gmail.com>
2023-06-08 10:24:56 +0000

    adding presta_channel_id column

--- models/intermediate/int_mapping_value.sql
+++ models/intermediate/int_mapping_value.sql
@@ -1,6 +1,7 @@
   SELECT 
   date_date,
   clean_map.name_sacem,
+  clean_map.presta_channel_id,
   channel_name_presta,
   type,
   ROUND(SUM(CASE WHEN type_droit LIKE 'DROIT EXECUTION PUBLIQUE' THEN val_sec ELSE 0 END),8) AS droit_execution_publique,
@@ -8,5 +9,5 @@
   ROUND(SUM(val_sec),8) as tot_val_sec
   FROM {{ source('projet_wagon', 'mapping_clean') }} clean_map
   LEFT JOIN {{ref("stg_data_sacem")}} sac USING (id_sacem)
-  GROUP BY 1,2,3,4
+  GROUP BY 1,2,3,4,5
   ORDER BY 1 DESC
