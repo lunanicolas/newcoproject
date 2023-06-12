dc995c4d8b947817dd21826d2b70d98c695331f6
flo coa <coaflorian@gmail.com>
2023-06-12 12:42:38 +0000

    editing mart_forecast

--- models/marts/mart_forecast_per_channel.sql
+++ models/marts/mart_forecast_per_channel.sql
@@ -12,6 +12,6 @@ clean.channel_name_presta,
 CASE 
   WHEN forecast IS NULL THEN ROUND(mean.avg_tot,8)
   ELSE ROUND(forecast,8) END AS forecast,
-FROM {{source("projet_wagon","mapping_clean")}}`rightsnow-385413.projet_wagon.mapping_clean` clean 
+FROM {{source("projet_wagon","mapping_clean")}} clean 
 LEFT JOIN {{source("projet_wagon","predictions_final")}} predict using(id_sacem)
 JOIN mean using(id_sacem)
