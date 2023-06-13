d0310b344ceee7e804d02924fc624d57195df30d
flo coa <coaflorian@gmail.com>
2023-06-13 08:41:17 +0000

    deleting mart first and mart revenue

--- models/luna/mart_first_semester_23_value.sql
+++ models/luna/mart_first_semester_23_value.sql
@@ -5,7 +5,7 @@ SELECT
     EXTRACT(MONTH FROM monitoring_date) as broadcast_month, 
     EXTRACT(YEAR FROM monitoring_date) as broadcast_year, 
     ROUND(SUM(revenue_per_diffusion),2) AS revenue
-  FROM {{ref('mart_revenue_per_diffusion')}}
+  FROM {{ref('mart_master_monitoring')}}
   WHERE user_category != 'rightsnow' AND DATE(monitoring_date) BETWEEN '2023-01-01' AND '2023-06-30' AND track_category != 'test_track'
-  GROUP BY user_id, email, user_category, EXTRACT(MONTH FROM monitoring_date), EXTRACT(YEAR FROM monitoring_date)
+  GROUP BY user_id, user_category, EXTRACT(MONTH FROM monitoring_date), EXTRACT(YEAR FROM monitoring_date)
 
