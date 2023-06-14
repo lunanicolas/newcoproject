fbcdbb5a694857cb86ed299683b5b150d0f6f3e1
laurent.heng@gmail.com <laurent.heng@gmail.com>
2023-06-14 09:44:59 +0000

    changing track_category

--- models/intermediate/int_tracks.sql
+++ models/intermediate/int_tracks.sql
@@ -8,7 +8,7 @@ track_id
 ,t.track_upload_date
 ,original_filename
 ,CASE
-    WHEN user_id = 156 OR user_id = 460 OR user_id = 366 OR user_id = 286 OR user_category = 'rightsnow' THEN 'test_track'
+    WHEN user_id = 156 OR user_id = 366 OR user_id = 286 OR user_category = 'rightsnow' THEN 'test_track'
     WHEN REGEXP_CONTAINS(original_filename, 'official|video|clip|orelsan|booba|gta') OR broadcast_count > 4000 THEN 'famous_track'
     ELSE 'emerging_track' 
 END AS track_category
